96008604680470287ba9f6516bd491efae7b7bf6
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-09-10 11:21:27 -0400

    use my fork of webcord-flake

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
       url = "github:the-argus/nixsys";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
+      inputs.webcord.url = "github:the-argus/webcord-flake";
     };
   };
 
